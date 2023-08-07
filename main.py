eec245b61ef9d9d0b9cb68d19da23e59be51b9f2
nooby124 <139121928+nooby124@users.noreply.github.com>
2023-08-07 19:00:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,100 @@
+import os
+import time
+import random
+import webbrowser
 
+print("welcome to ngen!")
+nofn = input("how many nitro links do you want to generate and check? (minimum is 10) ")
+#idk how to do this efficiently lol
+if nofn == "9":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "8":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "7":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "6":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "5":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "4":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "3":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "2":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "1":
+  print("error!")
+  print("the number is less then 10!")
+  time.sleep(0.6)
+  exit()
+if nofn == "0":
+  print("bruh")
+  time.sleep(0.6)
+  exit()
+os.system('cls')
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear_terminal()
+print("generating " + nofn + " nitro links.")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links..")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links...")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links.")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links..")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links...")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links.")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links..")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links...")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links.")
+time.sleep(1)
+clear_terminal()
+print("generating " + nofn + " nitro links..")
+time.sleep(1)
+nboat = ["1", "2", "3", "4", "5", "6", "7", "8", "9",]
+nonf = random.choice(nboat)
+print("found " + nonf + " nitro links!")
+#lesgo
+
+webbrowser.open('http://example.com')
